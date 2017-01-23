548efe65afe22c0fccb6e6c5bed30d84607322e5
Hugo Angeles <hugoangeles0810@gmail.com>
2017-01-23 01:32:49 -0500

    Adding assets

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,36 @@
 
 var app = {
     initialize: function() {
+
+        WORLD_BACKGROUND = '#000000';
+
+        HEIGHT  = document.documentElement.clientHeight;
+        WIDTH = document.documentElement.clientWidth;
+
         this.bindEvents();
     },
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        if ('addEventListener' in document) {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        }
     },
     onDeviceReady: function() {
+        app.startGame();
     },
+    startGame: function () {
+        function preload() {
+            game.stage.backgroundColor = WORLD_BACKGROUND;
+        }
+
+        function create () {
+
+        }
+
+        function update () {
+
+        }
+
+        var states = { preload: preload, create: create, update: update };
+        game = new Phaser.Game(WIDTH, HEIGHT, Phaser.CANVAS, 'phaser', states);
+    }
 };
